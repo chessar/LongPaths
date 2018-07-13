da89f6465cad56c06b38f2608e09027f17f2c765
Chessar <chessar@yandex.ru>
2018-07-13 16:33:15 +0300

    PVS-Studio Static Code Analyzer for AspNetForms example

--- Examples/AspNetForms/App_Code/Utils.cs
+++ Examples/AspNetForms/App_Code/Utils.cs
@@ -1,4 +1,6 @@
-﻿using Chessar;
+﻿// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+using Chessar;
 using System.Globalization;
 using System.IO;
 using System.Text;
